d5a8f0d1a820411653667c044cdf62605ce62372
TymonKolpowski <132841694+TymonKolpowski@users.noreply.github.com>
2025-02-24 08:22:04 +0100

    Update and rename lumache.py to tymonK.py

---
+++ tymonK.py
@@ -0,0 +1,16 @@
+"""
+TymonK - Fajny kolega.
+"""
+
+__version__ = "0.1.0"
+
+
+class Tak():
+    pass
+
+
+def hello_world():
+    """
+    Return hello world, tymon kołpowski
+    """
+    return "Hello world, Tymon Kołpowski
